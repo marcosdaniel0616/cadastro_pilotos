95b2fc6adfc4539376cf5f2b193cce910c32d8ba
Marcos Daniel <marcos.daniel0616@gmail.com>
2022-06-03 03:33:35 -0300

    tentiva de correção de erro.

--- cadastro_pilotos/settings.py
+++ cadastro_pilotos/settings.py
@@ -25,8 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f0kds-oor1=@dq3ic2q+!)qj-9@n!tw+$3zticxt=3i^hs9sw#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
